a885876c3dcc71c52207b167c420e2b50bab87a3
weihanyau <weihanyau@gmail.com>
2024-05-24 00:00:13 +0800

    add cross origin

--- src/main/java/com/example/bowbackend/controller/BowExecutionController.java
+++ src/main/java/com/example/bowbackend/controller/BowExecutionController.java
@@ -1,6 +1,7 @@
 package com.example.bowbackend.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.example.bowbackend.service.BowSequentialService;
 @RequestMapping("/bow")
 public class BowExecutionController {
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/sequential")
     public ResponseEntity<BowResultDTO> executeBowSequential(@RequestPart MultipartFile textFile) {
         if (!textFile.getOriginalFilename().endsWith(Constant.TEXT_FILE_SUFFIX)) {
@@ -28,6 +30,7 @@ public class BowExecutionController {
         return ResponseEntity.ok(BowSequentialService.executeBowSequential(words));
     }
 
+    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/concurrent/{methodType}")
     public ResponseEntity<BowResultDTO> executeBowConcurrent(@PathVariable("methodType") int methodType,
                                                              @RequestPart MultipartFile textFile) {
